0801465bb75f866f8512b39210649680e8237a38
jesperbrink <jesper@brinkandersen.dk>
2018-07-27 08:02:13 +0200

    Fixed error in test

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -9,7 +9,7 @@ func TestString(t *testing.T) {
 	program := &Program{
 		Statements: []Statement{
 			&VarStatement{
-				Token: token.Token{Type: token.LET, Literal: "var"},
+				Token: token.Token{Type: token.VAR, Literal: "var"},
 				Name: &Identifier{
 					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
 					Value: "myVar",
